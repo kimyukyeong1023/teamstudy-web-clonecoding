052cc6327ce424764c3ad29dd2009da1df9f7c23
kimyukyeong1023 <lkyip1023@gmail.com>
2026-08-02 19:38:44 +0900

    Disclaimer 추가

--- src/components/mainBanner/MainBanner.jsx
+++ src/components/mainBanner/MainBanner.jsx
@@ -27,6 +27,10 @@ export default function MainBanner() {
         </ul>
         <button className={styles.playBtn}></button>
       </div>
+      <div >
+        <p>※ 본 웹사이트는 상업적 목적이 아닌 개인 프론트엔드 학습용으로 제작된 클론 코딩 사이트입니다.</p>
+        <p>사이트 내 사용된 모든 이미지와 로고의 저작권 및 지적재산권은 <strong>현대자동차</strong>에 있으며, 문제 발생 시 즉각 삭제 조치하겠습니다.</p>
+      </div>
     </div>
   )
 }
