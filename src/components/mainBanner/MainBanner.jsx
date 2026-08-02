3b3547b448c43fcddcb0f9dfef0eb97d9ba17515
kimyukyeong1023 <lkyip1023@gmail.com>
2026-08-02 19:27:57 +0900

    화면 구조및 css생성

--- src/components/mainBanner/MainBanner.jsx
+++ src/components/mainBanner/MainBanner.jsx
@@ -4,7 +4,30 @@ import styles from './MainBanner.module.css';
 
 export default function MainBanner() {
   return (
-    <div >MainBanner</div>
+    <div className={styles.mianWrap}>
+      <div className={styles.titleBox}>
+        <h3>차이름</h3>
+        <p>차설명</p>
+
+      </div>
+      <div className={styles.bannerWrap}>
+        <button className={`${styles.bannerMoveBtn} ${styles.bannerBBtn}`}></button>
+        <span className={styles.BannerBNName}>이전차이름</span>
+        <img className={styles.bannerImg}
+              src="./mainBannerimg/Mainnew NEXO.avif" alt="차이미지" />
+        <span className={styles.BannerBNName}>다음차이름</span>
+        <button className={`${styles.bannerMoveBtn} ${styles.bannerNBtn}`}></button>
+      </div>
+      <div className={styles.listbar}>
+        <ul className={styles.listBnt}>
+          <li><button></button></li>
+          <li><button></button></li>
+          <li><button></button></li>
+          <li><button></button></li>
+        </ul>
+        <button className={styles.playBtn}></button>
+      </div>
+    </div>
   )
 }
 
